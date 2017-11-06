93e19d74b9c9c3092cc84bbd716c3d1dfaceeeab
Timi Wahalahti <timi.wahalahti@gmail.com>
2017-11-06 14:40:08 +0200

    Remoce custom css from customizer

--- inc/hooks.php
+++ inc/hooks.php
@@ -348,6 +348,18 @@ function air_helper_adminbar_show_env_styles() { ?>
 add_action( 'admin_head', 'air_helper_adminbar_show_env_styles' );
 
 /**
+ * Remove the additional CSS section, introduced in 4.7, from the Customizer.
+ * Add back by using `remove_action( 'customize_register', 'air_helper_customizer_remove_css_section' )`
+ *
+ * @param object 	$wp_customize WP_Customize_Manager.
+ * @since  1.3.0
+ */
+function air_helper_customizer_remove_css_section( $wp_customize ) {
+	$wp_customize->remove_section( 'custom_css' );
+}
+add_action( 'customize_register', 'air_helper_customizer_remove_css_section', 15 );
+
+/**
  * Allow Gravity Forms to hide labels to add placeholders.
  * Turn off by using `add_filter( 'gform_enable_field_label_visibility_settings', '__return_false' )`
  *
