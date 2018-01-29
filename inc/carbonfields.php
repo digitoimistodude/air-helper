539fe05c0e77031b910955de0c02674b933e52aa
Timi Wahalahti <timi.wahalahti@gmail.com>
2018-01-29 12:11:37 +0200

    add falsely removed CRB styles back

--- inc/carbonfields.php
+++ inc/carbonfields.php
@@ -7,10 +7,20 @@
  * @Author: Timi Wahalahti
  * @Date:   2017-09-21 10:31:31
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2018-01-29 11:34:51
+ * @Last Modified time: 2018-01-29 12:11:11
  *
  * @package airhelper
  */
+
+/**
+ *  Register and enqueue custom styles for CRB.
+ *  @since  1.5.0
+ */
+function air_helper_cf_styles() {
+	wp_register_style( 'air-helper-cf', air_helper_base_url() . '/assets/css/cf.css', false, filemtime( air_helper_base_path() . '/assets/css/cf.css' ) );
+	wp_enqueue_style( 'air-helper-cf' );
+}
+add_action( 'admin_enqueue_scripts', 'air_helper_cf_styles' );
 
 /**
  * Short-circuit (override) default get_post_metadata functionality with
