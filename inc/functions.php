8902eb26d6552fe4a9f36943f51a8e42e48aee21
Timi Wahalahti <timi.wahalahti@gmail.com>
2018-02-02 15:23:09 +0200

    add functions to get prev/next pages

--- inc/functions.php
+++ inc/functions.php
@@ -168,3 +168,89 @@ if ( ! function_exists( 'dude_get_crb_pll_id' ) ) {
 		return $condition;
 	}
 }
+
+if ( ! function_exists( 'get_next_page_id' ) ) {
+	/**
+	 *  Get ID of next page.
+	 *
+	 *  @since  1.5.1
+	 *  @param  integer $id page which next to get.
+	 *  @return mixed       false if no next page, id if there is.
+	 */
+	function get_next_page_id( $id = 0 ) {
+		$next_page_id = false;
+
+		// Get all pages under this section
+		$post = get_post( $id );
+		$get_pages = get_pages( array(
+			'child_of'		=> $post->post_parent,
+			'parent'			=> $post->post_parent,
+			'sort_column'	=> 'menu_order',
+			'sort_order'	=> 'asc',
+		) );
+
+		// Count pages.
+		$page_count = count( $get_pages );
+
+		for ( $p = 0; $p < $page_count; $p++ ) {
+			// Get the array key for our entry.
+			if ( $id === $get_pages[ $p ]->ID ) {
+				break;
+			}
+		}
+
+		// Assign our next key.
+		$next_key = $p + 1;
+
+		// If there isn't a value assigned for the previous key, go all the way to the end.
+		if ( isset( $get_pages[ $next_key ] ) ) {
+			$next_page_id = $get_pages[ $next_key ]->ID;
+		}
+
+		return $next_page_id;
+	}
+} // End if().
+
+if ( ! function_exists( 'get_prev_page_id' ) ) {
+	/**
+	 *  Get ID of previous page.
+	 *
+	 *  @since  1.5.1
+	 *  @param  integer $id page which previous to get.
+	 *  @return mixed       false if no previous page, id if there is.
+	 */
+	function get_prev_page_id( $id = 0) {
+		$prev_page_id = false;
+
+		// Get all pages under this section
+		$post = get_post( $id );
+		$get_pages = get_pages( array(
+			'child_of'		=> $post->post_parent,
+			'parent'			=> $post->post_parent,
+			'sort_column'	=> 'menu_order',
+			'sort_order'	=> 'asc',
+		) );
+
+		// Count pages.
+		$page_count = count( $get_pages );
+
+		for ( $p = 0; $p < $page_count; $p++ ) {
+			// Get the array key for our entry.
+			if ( $id === $get_pages[ $p ]->ID ) {
+				break;
+			}
+		}
+
+		// Assign our next key.
+		$prev_key = $p - 1;
+		$last_key = $page_count - 1;
+
+		// If there isn't a value assigned for the previous key, go all the way to the end.
+		if ( isset( $get_pages[ $prev_key ] ) ) {
+			$prev_page_id = $get_pages[ $prev_key ]->ID;
+		}
+
+		return $prev_page_id;
+	}
+} // End if().
+
