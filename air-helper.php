1f48bfc23d4c5fa71c93522eabc630bb7ac0fa7c
Timi Wahalahti <timi.wahalahti@gmail.com>
2018-02-05 13:14:33 +0200

    bump version 1.5.2

--- air-helper.php
+++ air-helper.php
@@ -3,7 +3,7 @@
  * Plugin Name: Air helper
  * Plugin URI: https://github.com/digitoimistodude/air-helper
  * Description: This plugin extends themes based on Air by adding useful hooks, functions and basic things for WooCommerce.
- * Version: 1.5.1
+ * Version: 1.5.2
  * Author: Digitoimisto Dude Oy, Timi Wahalahti
  * Author URI: https://www.dude.fi
  * Requires at least: 4.7
