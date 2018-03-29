9dcc8a3b9b9523423f37467e4dce93f7f4ae080e
Timi Wahalahti <timi.wahalahti@gmail.com>
2018-03-29 13:10:25 +0300

    orderby fix in init hook

--- inc/hooks.php
+++ inc/hooks.php
@@ -454,44 +454,54 @@ add_filter( 'wpseo_metabox_prio', 'air_helper_lowpriority_yoastseo' );
 add_filter( 'update_footer', '__return_empty_string', 11 );
 
 /**
- * Add support for correct UTF8 orderby for post_title (äöå).
+ * Add support for correct UTF8 orderby for post_title and term name (äöå).
+ * Turn off by using `remove_filter( 'init', 'air_helper_orderby_fix' )`
  * Props Teemu Suoranta https://gist.github.com/TeemuSuoranta/2174f78f37248aeef483526d1c5d176f
  *
  *  @since  1.5.0
  *  @param string $orderby ordering clause for query
  *  @return string ordering clause for query
  */
-add_filter( 'posts_orderby', function( $orderby ) use ( $wpdb ) {
-	if ( strstr( $orderby, 'post_title' ) ) {
-		$order        = ( strstr($orderby, 'post_title ASC' ) ? 'ASC' : 'DESC' );
-		$old_orderby  = $wpdb->posts . '.post_title ' . $order;
-		$utf8_orderby = 'CONVERT ( LCASE(' . $wpdb->posts . '.post_title) USING utf8) COLLATE utf8_bin ' . $order;
+function air_helper_orderby_fix() {
+	/**
+	 * Add support for correct UTF8 orderby for post_title and term name (äöå).
+	 *
+	 *  @since  1.5.0
+	 *  @param string $orderby ordering clause for query
+	 *  @return string ordering clause for query
+	 */
+	add_filter( 'posts_orderby', function( $orderby ) use ( $wpdb ) {
+		if ( strstr( $orderby, 'post_title' ) ) {
+			$order        = ( strstr($orderby, 'post_title ASC' ) ? 'ASC' : 'DESC' );
+			$old_orderby  = $wpdb->posts . '.post_title ' . $order;
+			$utf8_orderby = 'CONVERT ( LCASE(' . $wpdb->posts . '.post_title) USING utf8) COLLATE utf8_bin ' . $order;
 
-		// replace orderby clause in $orderby.
-		$orderby = str_replace( $old_orderby, $utf8_orderby, $orderby );
-	}
+			// replace orderby clause in $orderby.
+			$orderby = str_replace( $old_orderby, $utf8_orderby, $orderby );
+		}
 
-	return $orderby;
-} );
+		return $orderby;
+	} );
 
-/**
- * Add support for correct UTF8 orderby for term name (äöå).
- * Props Teemu Suoranta https://gist.github.com/TeemuSuoranta/2174f78f37248aeef483526d1c5d176f
- *
- *  @since  1.5.0
- *  @param string $orderby ordering clause for terms query
- *  @param array  $this_query_vars an array of terms query arguments
- *  @param array  $this_query_vars_taxonomy an array of taxonomies
- *  @return string ordering clause for terms query
- */
-add_filter( 'get_terms_orderby', function( $orderby, $this_query_vars, $this_query_vars_taxonomy ) {
-	if ( strstr( $orderby, 't.name' ) ) {
-		$old_orderby  = 't.name';
-		$utf8_orderby = 'CONVERT ( LCASE(t.name) USING utf8) COLLATE utf8_bin ';
+	/**
+	 * Add support for correct UTF8 orderby for term name (äöå).
+	 *
+	 *  @since  1.5.0
+	 *  @param string $orderby ordering clause for terms query
+	 *  @param array  $this_query_vars an array of terms query arguments
+	 *  @param array  $this_query_vars_taxonomy an array of taxonomies
+	 *  @return string ordering clause for terms query
+	 */
+	add_filter( 'get_terms_orderby', function( $orderby, $this_query_vars, $this_query_vars_taxonomy ) {
+		if ( strstr( $orderby, 't.name' ) ) {
+			$old_orderby  = 't.name';
+			$utf8_orderby = 'CONVERT ( LCASE(t.name) USING utf8) COLLATE utf8_bin ';
 
-		// replace orderby clause in $orderby.
-		$orderby = str_replace( $old_orderby, $utf8_orderby, $orderby );
-	}
+			// replace orderby clause in $orderby.
+			$orderby = str_replace( $old_orderby, $utf8_orderby, $orderby );
+		}
 
-	return $orderby;
-}, 10, 3);
+		return $orderby;
+	}, 10, 3);
+}
+add_filter( 'init', 'air_helper_orderby_fix' );
