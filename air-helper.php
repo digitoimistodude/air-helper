125ac1a471d5993fabec936e1937856c7eed2af1
Timi Wahalahti <timi.wahalahti@gmail.com>
2018-12-04 15:28:00 +0200

    introduce first version of dashboard widget

--- air-helper.php
+++ air-helper.php
@@ -163,6 +163,7 @@ function air_helper_fly() {
 	require_once air_helper_base_path() . '/inc/functions.php';
 	require_once air_helper_base_path() . '/inc/misc.php';
 	require_once air_helper_base_path() . '/inc/post-meta-revisions.php';
+	require_once air_helper_base_path() . '/inc/dashboard.php';
 }
 add_action( 'init', 'air_helper_fly', 999 );
 
---
+++ assets/js/dashboard-widget.js
@@ -0,0 +1,68 @@
+/*
+* @Author: Timi Wahalahti
+* @Date:   2018-12-04 13:35:35
+* @Last Modified by:   Timi Wahalahti
+* @Last Modified time: 2018-12-04 15:22:41
+*/
+
+jQuery(document).ready( function($) {
+	jQuery( '#dashboard-widgets #air-helper-help .support-form form' ).submit( function(e) {
+		e.preventDefault();
+
+		var send = true;
+
+		// reset field styles
+		$(this).find( 'input[name="subject"]' ).removeClass( 'error' );
+		$(this).find( 'textarea' ).removeClass( 'error' );
+
+		// content
+		var subject = $(this).find( 'input[name="subject"]' ).val()
+		var content = $(this).find( 'textarea' ).val();
+
+		// check if subject is empty
+		if ( ! $.trim( subject ) ) {
+			$(this).find( 'input[name="subject"]' ).addClass( 'error' );
+			send = false;
+		}
+
+		// check if content is empty
+		if ( ! $.trim( content) ) {
+			$(this).find( 'textarea' ).addClass( 'error' );
+			send = false;
+		}
+
+		// fields are empty, show error message
+		if ( ! send ) {
+			$('#dashboard-widgets #air-helper-help .support-form p.message-field-error').show();
+		} else {
+			// send
+
+			// reset messages
+			$('#dashboard-widgets #air-helper-help .support-form p.message-field-error').hide();
+			$(this).find( 'input[name="subject"]' ).removeClass( 'error' );
+			$(this).find( 'textarea' ).removeClass( 'error' );
+
+			// data to send
+	 	  var data = {
+				'action': 'air_helper_send_ticket',
+				'subject': subject,
+				'content': content,
+				'ticket_nonce': $(this).find( 'input[name="nonce"]' ).val(),
+			};
+
+			// send the data
+			// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+			jQuery.post(ajaxurl, data, function(response) {
+				if ( response.success ) {
+					$('#dashboard-widgets #air-helper-help .support-form form' ).hide();
+					$('#dashboard-widgets #air-helper-help .support-form p.message-error').hide();
+					$('#dashboard-widgets #air-helper-help .support-form p.message-success').show();
+				} else {
+					$('#dashboard-widgets #air-helper-help .support-form p.message-error').show();
+				}
+			} ).fail( function() {
+    		$('#dashboard-widgets #air-helper-help .support-form p.message-error').show();
+  		} );
+		}
+	} );
+} );
