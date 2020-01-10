2ef1f9bec79756aad8c2439f72a4acbf66528aa4
Timi Wahalahti <timi@wahalahti.fi>
2020-01-10 16:18:53 +0200

    move update related to own file

---
+++ inc/admin/updates.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * WordPress update related actions.
+ *
+ * @Author: Timi Wahalahti
+ * @Date:   2020-01-10 16:17:20
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2020-01-10 16:18:13
+ *
+ * @package air-helper
+ */
+
+/**
+ * Hide WP updates nag.
+ * Turn off by using `remove_action( 'admin_menu', 'air_helper_wphidenag' )`
+ *
+ * @since  0.1.0
+ */
+add_action( 'admin_menu', 'air_helper_wphidenag' );
+function air_helper_wphidenag() {
+  remove_action( 'admin_notices', 'update_nag', 3 );
+} // end air_helper_wphidenag
