7f8517c956a607e4f874228e3d4834a82fa0e40e
Timi Wahalahti <timi@wahalahti.fi>
2020-01-10 16:27:35 +0200

    own file for customizer related things

--- inc/hooks.php
+++ inc/hooks.php
@@ -43,18 +43,6 @@ function air_helper_helper_disable_emojicons_tinymce( $plugins ) {
 }
 
 /**
- * Remove the additional CSS section, introduced in 4.7, from the Customizer.
- * Add back by using `remove_action( 'customize_register', 'air_helper_customizer_remove_css_section' )`
- *
- * @param object $wp_customize WP_Customize_Manager.
- * @since  1.3.0
- */
-function air_helper_customizer_remove_css_section( $wp_customize ) {
-	$wp_customize->remove_section( 'custom_css' );
-}
-add_action( 'customize_register', 'air_helper_customizer_remove_css_section', 15 );
-
-/**
  *  Show TinyMCE second editor tools row by default.
  *  Turn off by using `remove_filter( 'tiny_mce_before_init', 'air_helper_show_second_editor_row' )`
  *
