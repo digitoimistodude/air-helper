c757c77fa50b75704f4cbc83d4bd41dea3231ab1
Timi Wahalahti <timi@wahalahti.fi>
2020-02-11 12:01:38 +0200

    hide WP about and imagify from admin bar

--- inc/admin/adminbar.php
+++ inc/admin/adminbar.php
@@ -5,13 +5,14 @@
  * @Author: Timi Wahalahti
  * @Date:   2020-01-10 16:14:34
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2020-01-10 16:25:59
+ * @Last Modified time: 2020-02-11 12:01:19
  *
  * @package air-helper
  */
 
 /**
  * Clean up admin bar.
+ *
  * Turn off by using `remove_action( 'wp_before_admin_bar_render', 'air_helper_helper_remove_admin_bar_links' )`
  * Modify list by using `add_filter( 'air_helper_helper_remove_admin_bar_links', 'myprefix_override_air_helper_helper_remove_admin_bar_links' )`
  *
@@ -22,6 +23,7 @@ function air_helper_helper_remove_admin_bar_links() {
   global $wp_admin_bar;
 
   $remove_items = apply_filters( 'air_helper_helper_remove_admin_bar_links', array(
+    'wp-logo',
     'about',
     'wporg',
     'documentation',
@@ -29,6 +31,7 @@ function air_helper_helper_remove_admin_bar_links() {
     'feedback',
     'updates',
     'comments',
+    'imagify',
   ) );
 
   foreach ( $remove_items as $item ) {
