4f173225247bd3e537e9b897b5c2d7607e86f258
Timi Wahalahti <timi@wahalahti.fi>
2020-02-11 11:48:42 +0200

    add caching to get_next_page_id and get_prev_page_id, default to current page

--- functions/pagination.php
+++ functions/pagination.php
@@ -5,7 +5,7 @@
  * @Author: Timi Wahalahti
  * @Date:   2020-01-10 15:47:21
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2020-01-10 15:49:15
+ * @Last Modified time: 2020-02-11 11:48:12
  *
  * @package air-helper
  */
@@ -15,21 +15,33 @@ if ( ! function_exists( 'get_next_page_id' ) ) {
    *  Get ID of next page.
    *
    *  @since  1.5.1
-   *  @param  integer $id page which next to get.
-   *  @return mixed       false if no next page, id if there is.
+   *  @param  integer $id Page which next to get, defaults to current page.
+   *  @return mixed       False if no next page, id if there is.
    */
   function get_next_page_id( $id = 0 ) {
+    if ( empty( $id ) ) {
+      $id = get_the_id();
+    }
+
+    $cache_key = apply_filters( 'get_next_page_id_cache_key', "get_next_page_id_for_{$id}", $id );
+
+    // Check if result is cached and if, return cached version
+    $next_page_id = get_transient( $cache_key );
+    if ( ! empty( $next_page_id ) ) {
+      return $next_page_id;
+    }
+
     $next_page_id = false;
 
     // Get all pages under this section
     $post = get_post( $id );
-    $get_pages = get_pages( array(
+    $get_pages = get_pages( [
       'post_type'   => $post->post_type,
       'child_of'    => $post->post_parent,
       'parent'      => $post->post_parent,
       'sort_column' => 'menu_order',
       'sort_order'  => 'asc',
-    ) );
+    ] );
 
     // Count pages.
     $page_count = count( $get_pages );
@@ -49,6 +61,9 @@ if ( ! function_exists( 'get_next_page_id' ) ) {
       $next_page_id = $get_pages[ $next_key ]->ID;
     }
 
+    // Save to cache
+    set_transient( $cache_key, $next_page_id, apply_filters( 'get_next_page_id_cache_lifetime', MINUTE_IN_SECONDS * 30 ) );
+
     return $next_page_id;
   } // end get_next_page_id
 } // end if
@@ -58,21 +73,33 @@ if ( ! function_exists( 'get_prev_page_id' ) ) {
    *  Get ID of previous page.
    *
    *  @since  1.5.1
-   *  @param  integer $id page which previous to get.
-   *  @return mixed       false if no previous page, id if there is.
+   *  @param  integer $id Page which previous to get, defaults to current page.
+   *  @return mixed       False if no previous page, id if there is.
    */
   function get_prev_page_id( $id = 0 ) {
+    if ( empty( $id ) ) {
+      $id = get_the_id();
+    }
+
+    $cache_key = apply_filters( 'get_prev_page_id_cache_key', "get_prev_page_id_for_{$id}", $id );
+
+    // Check if result is cached and if, return cached version
+    $prev_page_id = get_transient( $cache_key );
+    if ( ! empty( $prev_page_id ) ) {
+      return $prev_page_id;
+    }
+
     $prev_page_id = false;
 
     // Get all pages under this section
     $post = get_post( $id );
-    $get_pages = get_pages( array(
+    $get_pages = get_pages( [
       'post_type'   => $post->post_type,
       'child_of'    => $post->post_parent,
       'parent'      => $post->post_parent,
       'sort_column' => 'menu_order',
       'sort_order'  => 'asc',
-    ) );
+    ] );
 
     // Count pages.
     $page_count = count( $get_pages );
@@ -92,6 +119,9 @@ if ( ! function_exists( 'get_prev_page_id' ) ) {
     if ( isset( $get_pages[ $prev_key ] ) ) {
       $prev_page_id = $get_pages[ $prev_key ]->ID;
     }
+
+    // Save to cache
+    set_transient( $cache_key, $next_page_id, apply_filters( 'get_prev_page_id_cache_lifetime', MINUTE_IN_SECONDS * 30 ) );
 
     return $prev_page_id;
   } // end get_prev_page_id
