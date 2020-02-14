440db0e9760b9d5468ebc6fc9f52dfad4186d735
Timi Wahalahti <timi@wahalahti.fi>
2020-02-14 11:18:55 +0200

    chnage upload path options only once

--- inc/media.php
+++ inc/media.php
@@ -5,7 +5,7 @@
  * @Author: Timi Wahalahti
  * @Date:   2020-01-10 16:42:40
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2020-02-11 14:58:29
+ * @Last Modified time: 2020-02-14 11:16:54
  *
  * @package air-helper
  */
@@ -18,8 +18,12 @@
  * @since 0.1.0
  */
 if ( apply_filters( 'air_helper_change_uploads_path', true ) ) {
-  update_option( 'upload_path', untrailingslashit( str_replace( 'wp', 'media', ABSPATH ) ) );
-  update_option( 'upload_url_path', untrailingslashit( str_replace( 'wp', 'media', get_site_url() ) ) );
-  define( 'uploads', '' . 'media' );
+  if ( ! get_option( 'air_helper_changed_uploads_path' ) ) {
+    update_option( 'upload_path', untrailingslashit( str_replace( 'wp', 'media', ABSPATH ) ) );
+    update_option( 'upload_url_path', untrailingslashit( str_replace( 'wp', 'media', get_site_url() ) ) );
+    update_option( 'air_helper_changed_uploads_path', date_i18n( 'Y-m-d H:i:s' ) );
+  } // end option update
+
+  define( 'uploads', 'media' ); // phpcs:ignore Generic.NamingConventions.UpperCaseConstantName.ConstantNotUpperCase
   add_filter( 'option_uploads_use_yearmonth_folders', '__return_false', 100 );
-}
+} // end filter if
