4e4e0a252243cd8b7fd9e7c5088bf7e2ebeb967e
Timi Wahalahti <timi@wahalahti.fi>
2020-03-12 13:33:24 +0200

    update upload options always unless in production and already updated

--- inc/media.php
+++ inc/media.php
@@ -5,7 +5,7 @@
  * @Author: Timi Wahalahti
  * @Date:   2020-01-10 16:42:40
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2020-02-14 11:16:54
+ * @Last Modified time: 2020-03-12 13:31:48
  *
  * @package air-helper
  */
@@ -18,7 +18,13 @@
  * @since 0.1.0
  */
 if ( apply_filters( 'air_helper_change_uploads_path', true ) ) {
-  if ( ! get_option( 'air_helper_changed_uploads_path' ) ) {
+  $update_option = true;
+
+  if ( 'production' === getenv( 'WP_ENV' ) && get_option( 'air_helper_changed_uploads_path' ) ) {
+    $update_option = false;
+  }
+
+  if ( $update_option ) {
     update_option( 'upload_path', untrailingslashit( str_replace( 'wp', 'media', ABSPATH ) ) );
     update_option( 'upload_url_path', untrailingslashit( str_replace( 'wp', 'media', get_site_url() ) ) );
     update_option( 'air_helper_changed_uploads_path', date_i18n( 'Y-m-d H:i:s' ) );
