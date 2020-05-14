42e0bfa32f9afed2f885ea253dbc2cfc3c354e1d
Roni Laukkarinen <roni@dude.fi>
2020-05-14 11:09:32 +0300

    Release 2.1.4

--- air-helper.php
+++ air-helper.php
@@ -3,7 +3,7 @@
  * Plugin Name: Air helper
  * Plugin URI: https://github.com/digitoimistodude/air-helper
  * Description: Plugin provides helpful functions and modifications for WordPress projects.
- * Version: 2.1.3
+ * Version: 2.1.4
  * Author: Digitoimisto Dude Oy, Timi Wahalahti
  * Author URI: https://www.dude.fi
  * Requires at least: 5.0
