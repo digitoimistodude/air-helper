a27dda8b3eff652d2be83d27672b9f25450df118
Timi Wahalahti <timi@wahalahti.fi>
2021-09-23 14:32:16 +0300

    Remove plugins page from multisite admin menu

--- inc/admin/access.php
+++ inc/admin/access.php
@@ -5,7 +5,7 @@
  * @Author: Timi Wahalahti
  * @Date:   2020-01-10 16:15:47
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2021-08-03 10:00:03
+ * @Last Modified time: 2021-09-23 12:13:44
  *
  * @package air-helper
  */
@@ -74,3 +74,29 @@ function air_helper_maybe_remove_plugins_from_admin_menu( $menu_links ) {
 
   return $menu_links;
 } // end air_helper_maybe_remove_plugins_from_admin_menu
+
+/**
+ * Remove plugins page from multisite admin menu, execpt for users with spesific domain or override in user meta.
+ *
+ * Turn off by using `remove_filter( 'admin_bar_menu', 'air_helper_maybe_remove_plugins_from_network_admin_menu', 999 )`
+ *
+ * @since 2.11.0
+ */
+add_action( 'admin_bar_menu', 'air_helper_maybe_remove_plugins_from_network_admin_menu', 999 );
+function air_helper_maybe_remove_plugins_from_network_admin_menu() {
+  $current_user = get_current_user_id();
+  $user = new WP_User( $current_user );
+  $domain = apply_filters( 'air_helper_dont_remove_plugins_admin_menu_link_from_domain', 'dude.fi' );
+  $meta_override = get_user_meta( $user->ID, '_airhelper_admin_show_plugins', true );
+
+  if ( strpos( $user->user_email, "@{$domain}" ) !== false ) {
+    return;
+  }
+
+  if ( 'true' === $meta_override ) {
+    return;
+  }
+
+  global $wp_admin_bar;
+  $wp_admin_bar->remove_node( 'network-admin-p' );
+} // end air_helper_maybe_remove_plugins_from_network_admin_menu
