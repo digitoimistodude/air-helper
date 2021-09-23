8c83e5a9996ce93ca9f824bcd932a8a7e7b996e2
Timi Wahalahti <timi@wahalahti.fi>
2021-09-23 14:43:39 +0300

    If ACF Pro key is defined in .env file, try to activate the license automatically

--- inc/admin/acf.php
+++ inc/admin/acf.php
@@ -5,7 +5,7 @@
  * @Author: Timi Wahalahti
  * @Date:   2020-01-10 16:11:23
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2020-02-11 11:59:25
+ * @Last Modified time: 2021-09-23 14:42:43
  *
  * @package air-helper
  */
@@ -34,3 +34,40 @@ function air_helper_maybe_hide_acf() {
 
   return true;
 } // end air_helper_maybe_hide_acf
+
+/**
+ * If ACF Pro license key is defined in .env file, try to load the key from there
+ * and activate ACF if not already activated or activation is corrupted.
+ *
+ * Turn off by using `remove_action( 'admin_init', 'air_helper_get_acf_pro_license_from_env' )`
+ *
+ * @since 2.11.0
+ */
+add_action( 'admin_init', 'air_helper_get_acf_pro_license_from_env' );
+function air_helper_get_acf_pro_license_from_env() {
+  // Bail if no ACF
+  if ( ! function_exists( 'acf_pro_get_license_key' ) ) {
+    return;
+  }
+
+  // Bail if no ACF
+  if ( ! method_exists( 'ACF_Admin_Updates', 'activate_pro_licence' ) ) {
+    return;
+  }
+
+  // Bail if no license key in .env
+  if ( empty( getenv( 'ACF_PRO_KEY' ) ) ) {
+    return;
+  }
+
+  // Bail if license key is valid
+  if ( acf_pro_get_license_key() ) {
+    return;
+  }
+
+  // Force our license key into POST data, as ACF reads it from there
+  $_POST['acf_pro_licence'] = getenv( 'ACF_PRO_KEY' );
+
+  // Run ACF function to activate the license
+  ACF_Admin_Updates::activate_pro_licence();
+} // end air_helper_get_acf_pro_license_from_env
