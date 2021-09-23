3e6e5a15cfca8425502ed3c03fa90b7318b4b87b
Timi Wahalahti <timi@wahalahti.fi>
2021-09-23 14:32:26 +0300

    Mailhog support for development enviroments

--- inc/priority/mail-delivery.php
+++ inc/priority/mail-delivery.php
@@ -5,7 +5,7 @@
  * @Author: Timi Wahalahti
  * @Date:   2020-01-10 16:40:38
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2021-08-03 11:12:53
+ * @Last Modified time: 2021-09-23 13:44:50
  *
  * @package air-helper
  */
@@ -34,6 +34,16 @@ if ( apply_filters( 'air_helper_mail_delivery', true ) && apply_filters( 'air_he
     define( 'SENDGRID_CATEGORIES', sanitize_title( get_option( 'blogname' ) ) );
     define( 'SENDGRID_STATS_CATEGORIES', sanitize_title( get_option( 'blogname' ) ) );
   }
+
+  // Mailhog support for development enviroments.
+  if ( 'development' === wp_get_environment_type() ) {
+    add_action( 'phpmailer_init', function( $phpmailer ) {
+      $phpmailer->Host = ! empty( getenv( 'WP_MAILHOG_HOST' ) ) ? getenv( 'WP_MAILHOG_HOST' ) : '127.0.0.1';
+      $phpmailer->Port = ! empty( getenv( 'WP_MAILHOG_POST' ) ) ? getenv( 'WP_MAILHOG_POST' ) : '1025';
+      $phpmailer->SMTPAuth = false;
+      $phpmailer->isSMTP();
+    } );
+  }
 } // end if
 
 /**
@@ -48,6 +58,11 @@ function air_helper_mail_delivery_check() {
 
   // SendGrid for legacy support.
   if ( is_plugin_active( 'sendgrid-email-delivery-simplified/wpsendgrid.php' ) && getenv( 'SENDGRID_API_KEY' ) ) {
+    return true;
+  }
+
+  // Do not show the notice in dev.
+  if ( 'development' === wp_get_environment_type() ) {
     return true;
   }
 
