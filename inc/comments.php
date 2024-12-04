1a43fd08e7ebfc61467d760ce671d63175537e4d
Roni Laukkarinen <roni@dude.fi>
2024-12-04 20:57:24 +0200

    Change comment notification recipient to admin email instead of post author, T-18096

--- inc/comments.php
+++ inc/comments.php
@@ -37,3 +37,19 @@ function air_helper_remove_recent_comments_style() {
   global $wp_widget_factory;
   remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
 } // end air_helper_remove_recent_comments_style
+
+/**
+ * Change comment notification recipient to admin email instead of post author. (T-18096)
+ *
+ * Turn off by using `remove_filter('comment_notification_recipients', 'air_helper_modify_comment_notification_recipient')`
+ *
+ * @since 3.1.2
+ * @param array  $emails     An array of email addresses to receive a comment notification.
+ * @param string $comment_id The comment ID as a numeric string.
+ * @return array Modified array of notification recipients
+ */
+add_filter( 'comment_notification_recipients', 'air_helper_modify_comment_notification_recipient', 10, 2 );
+function air_helper_modify_comment_notification_recipient( $emails, $comment_id ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+  // Replace all notification recipients with admin email
+  return array( get_option( 'admin_email' ) );
+}
